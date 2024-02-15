5c7aa99146a7357dbc646cd14f13d42a6c88bd46
hubgx <h.menou@gridx.de>
2024-02-15 14:55:25 +0100

    feat: basic setup

--- gxdep.sh
+++ gxdep.sh
@@ -4,5 +4,7 @@ echo "input1:" $1
 TARGET_GB_SN=$(echo $1 | grep -E "$GBX_SN")
 echo "serial number:" $TARGET_GB_SN
 
+gxctl get deploy -S $TARGET_GB_SN
+echo "Received data from gxctl:" $DEP_DATA
 DEP_DATA=$(gxctl get deploy -S $TARGET_GB_SN | grep -E "monitoring" | grep -E "^( )*[a-zA-Z0-9][a-zA-Z0-9\-]*" -o)
 echo $DEP_DATA
